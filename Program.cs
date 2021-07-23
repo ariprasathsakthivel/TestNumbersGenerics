3aeb664f357b0c0784071650f977ba0d090aca11
ariprasathsakthivel <67471289+ariprasathsakthivel@users.noreply.github.com>
2021-07-23 16:59:56 +0530

    [Ariprasath] added: Find the maximum number from the three numbers. numbers are of datatype intergers

--- Program.cs
+++ Program.cs
@@ -6,7 +6,22 @@ namespace TestMaximum
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Maximum number finder program");
+            MaxofThree(10,2, 11);
+        }
+        public static void MaxofThree(int num1,int num2,int num3)
+        {
+            if (num1.CompareTo(num2)>0 && num1.CompareTo(num3)>0)
+            {
+                Console.WriteLine("number1 is the maximum number");
+            }
+            else if (num2.CompareTo(num1)>0 && num2.CompareTo(num3)>0)
+            {
+                Console.WriteLine("number2 is the maximum number");
+            }
+            else
+            {
+                Console.WriteLine("number3 is the maximum number");
+            }
         }
     }
 }
