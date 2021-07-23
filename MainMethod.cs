7f35cc716157a9654db9a6d6875afe5760513920
ariprasathsakthivel <67471289+ariprasathsakthivel@users.noreply.github.com>
2021-07-23 21:29:14 +0530

    [Ariprasath] added: used generics at class level to pass multiple different types of values for finding the maximum value of three values provided.

---
+++ MainMethod.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestMaximum
+{
+    class MainMethod
+    {
+        static void Main(string[] args)
+        {
+            TestMaximumGenerics<int> numbers = new TestMaximumGenerics<int>(10, 9, 8);
+            numbers.MaxofThree();
+            TestMaximumGenerics<double> decimalValues = new TestMaximumGenerics<double>(10.2, 10.3, 10.1);
+            decimalValues.MaxofThree();
+            TestMaximumGenerics<string> characterCollection = new TestMaximumGenerics<string>("Apple", "Peach", "Banana");
+            characterCollection.MaxofThree();
+        }
+    }
+}
---
+++ TestMaximumGenerics.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestMaximum
+{
+    class TestMaximumGenerics<T> where T : IComparable<T>
+    {
+        T value1, value2, value3;
+        public TestMaximumGenerics(T value1,T value2, T value3)
+        {
+            this.value1 = value1;
+            this.value2 = value2;
+            this.value3 = value3;
+        }
+        public void MaxofThree()
+        {
+            if (this.value1.CompareTo(this.value2) > 0 && this.value1.CompareTo(this.value3) > 0)
+            {
+                Console.WriteLine("value1 is the maximum number");
+            }
+            else if (this.value2.CompareTo(this.value1) > 0 && this.value2.CompareTo(this.value3) > 0)
+            {
+                Console.WriteLine("value2 is the maximum number");
+            }
+            else
+            {
+                Console.WriteLine("value3 is the maximum number");
+            }
+        }
+    }
+}
